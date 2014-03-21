f50446c638c20d916c8e6e712990e10d4d91f149
pmcote <pmcote1@gmail.com>
2014-03-21 13:32:49 -0400

    tracking some working changes! hooray

--- Routes/index.js
+++ Routes/index.js
@@ -1,5 +1,9 @@
 //Code on homepage, give info to action script
 
+
+
+
+
 exports.index = function (req, res){
 	res.render('index', {title: 'BMO_app'});
 }
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express')
     , octo = require('octopart')
     , Converter = require('csvtojson').core.Converter
+    , routes = require('./routes')
+    , path = require('path')
+    , fs = require('fs')
+
 
 
 var app = express();
@@ -9,13 +13,18 @@ app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.logger('dev'));
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'))
 })
 
 app.get('/', routes.index)
+app.get('/uploadCSV', function (req , res) {
+	console.log('File name is' + req.files.myCSV.name);
+})
 
 var server = app.listen(3000)
 
 
 //Flow - iter for each break point, Check if break point < stock print
 
-//Referenced sites:
+//Referenced sites: http://howtonode.org/really-simple-file-uploads
