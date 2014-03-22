7d29e0d4a0aae13ad224f8799738f63f58f53ad3
pmcote <pmcote1@gmail.com>
2014-03-22 15:51:25 -0400

    file upload working, whoooo

--- app.js
+++ app.js
@@ -22,16 +22,48 @@ app.configure(function(){
 })
 
 app.get('/', routes.index)
-app.post('/uploadCSV', function(req, res) {
-	console.log(req.body);
+app.post('/uploadCSV', function (req, res) { 
+	//get the file name
 	console.log(req.files)
-})
+	var filename=req.files.myCSV.name;
+	var maxSizeOfFile=100;
+	var msg="";
+
+	// get the temporary location of the file
+    var tmp_path = req.files.myCSV.path;
+    
+	// set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = __dirname +'/upload/' + req.files.myCSV.name;
+
+	if ((req.files.myCSV.size /1024 )< maxSizeOfFile){
+		fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+			fs.unlink(tmp_path, function() {
+				if (err) throw err; 
+			});
+		});
+		msg="File uploaded sucessfully"
+	}else{
+	// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+		fs.unlink(tmp_path, function(err) {
+            if (err) throw err; 
+        });
+		msg="File upload failed.File extension not allowed and size must be less than "+maxSizeOfFile;
+	}
+	 res.end(msg);                                      
+});   
+
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 
 //Flow - iter for each break point, Check if break point < stock print
 
-//Referenced sites: http://howtonode.org/really-simple-file-uploads :)
+//Referenced sites: 
+//For file upload, after failing with like 100 other methods https://github.com/Tutorialindustry/node.js/tree/master/node.js-file-upload-tutorial
+
+
+
