4d7b40bcfcc85bf9a5f6395fb3b01acb8a39e538
pmcote <pmcote1@gmail.com>
2014-03-22 20:48:37 -0400

    added basic CSV parsing capabilities

--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,5 @@
 //Code on homepage, give info to action script
 
-
-
-
-
 exports.index = function (req, res){
 	res.render('index', {title: 'BMO_app'});
 }
--- Routes/optimize.js
+++ Routes/optimize.js
@@ -2,6 +2,38 @@
 //break point/inventory >> display
 
 //Parse CSV for queries (we want the Value and the Type)
+//var csv is the CSV file with headers
+var fs = require('fs')
+//Converter Class
+var Converter=require("csvtojson").core.Converter;
+
+//CSV File Path or CSV String or Readable Stream Object
+var csvFileName=fs.readFileSync('./test-bom.csv', 'utf8');
+
+//new converter instance
+var csvConverter=new Converter();
+
+//end_parsed will be emitted once parsing finished
+csvConverter.on("end_parsed",function(jsonObj){
+   console.log(jsonObj); //here is your result json object
+   console.log(jsonObj.csvRows[0].Value) 
+   console.log(jsonObj.csvRows.length)
+
+   var RefDes = [];
+   var Value = [];
+   var Type = [];
+
+   for (var i = 0;i<jsonObj.csvRows.length;i++) {
+   		RefDes.push(jsonObj.csvRows[i].RefDes)
+   		Value.push(jsonObj.csvRows[i].Value)
+   		Type.push(jsonObj.csvRows[i].Type)
+   		console.log(RefDes, Value, Type)
+   }
+});
+
+//read from file
+csvConverter.from(csvFileName);
+
 
 
 //Pass as a API search endpoint and grab the JSON output
